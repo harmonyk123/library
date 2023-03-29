680824608ef8580f7b8f25821ed3f83a917c73ee
HarmonyKerry <50801720+harmonyk123@users.noreply.github.com>
2023-03-29 20:15:55 +0100

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-localStorage.setItem('patience','you must have, my young padawan');
-
-console.log(localStorage)
-
-let yoda = localStorage.getItem('Patience you must have, my young padawan');
-console.log(yoda)
